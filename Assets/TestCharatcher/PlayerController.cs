b168be4678be71c5059fd3858a8ef6de03d6a156
Szakács Olivér <oliverszkcs@gmail.com>
2024-02-02 13:52:55 +0100

    Player controller with bug

--- Assets/TestCharatcher/PlayerController.cs
+++ Assets/TestCharatcher/PlayerController.cs
@@ -2,43 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class PlayerController : MonoBehaviour{
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private float speed = 1;
-    [SerializeField] private float turnSpeed = 1000;
-    [SerializeField] Camera camera;
-    [SerializeField] private Vector3 offset = Vector3.zero;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    [SerializeField] private float angularVelocity; //    fok / masodperc
+    private Rigidbody rigidbody;
 
-    private Vector3 input;
-
-    private void Update(){
-        GatherInput();
-        Look();
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate(){
-        Move();
+    private Vector2 GetInput()
+    {
+        Vector2 input = Vector2.zero;
+        input.x = -Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        return input;
+    }
+    private float AngleConverter(float angle) //-180 180
+    {
+        if(angle < 0)
+        {
+            angle = 360 + angle;
+        }
+        return angle;
+    }
+    private float GetRotationSign(Vector2 input)
+    {
+        float angle = AngleConverter(Vector2.SignedAngle(Vector2.one, input));
+        float characterRotationY =rigidbody.transform.rotation.eulerAngles.y;
+        float diff =  angle - characterRotationY ;
+        float rotationSign = (angle - characterRotationY) / Mathf.Abs(angle - characterRotationY);
+        
+        if(diff > 180 && angle > characterRotationY)
+        {
+            rotationSign *=-1;
+        }
+        Debug.Log(angle + " " + characterRotationY + " " + diff + " "+rotationSign);
+        return rotationSign;
+    }
+    
+    private void Update()
+    {
+       Vector2 input= GetInput();
+        if (input.magnitude != 0)
+        {
+            rigidbody.transform.rotation=(Quaternion.Euler(0, rigidbody.transform.rotation.eulerAngles.y + GetRotationSign(input) *angularVelocity * Time.deltaTime, 0));
+        }
+
     }
 
-    private void GatherInput(){
-        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-    }
+    
 
-    private void Look(){
-        if (input == Vector3.zero) { return; } 
-        var rot = Quaternion.LookRotation(input.ToIso(), Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
-    }
-
-    private void Move(){
-        Vector3 position = transform.position + (transform.forward * input.normalized.magnitude * speed * 0.25f * Time.deltaTime);
-        rb.MovePosition(position);
-        camera.transform.SetPositionAndRotation(transform.position + offset , camera.transform.rotation);
-    }
 }
-
-public static class Helpers{
-    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
-    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
-}
+    
